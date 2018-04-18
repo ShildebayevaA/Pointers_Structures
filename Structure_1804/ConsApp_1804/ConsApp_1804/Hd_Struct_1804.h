c2b14ebaf6f1fd6a809449218c18133765fde2f1
ShildebayevaA <33974039+ShildebayevaA@users.noreply.github.com>
2018-04-18 06:03:27 +0600

    Add files via upload

---
+++ Structure_1804/ConsApp_1804/ConsApp_1804/Hd_Struct_1804.h
@@ -0,0 +1,50 @@
+struct Hockey
+{
+	char FIO;
+	int vozrast;
+	int cnt_games;
+	int cnt_goals;
+};
+
+struct Tovar
+{
+	char entitle;
+	char vendor;
+	int man_year;
+	int	quantity;
+	int price;
+};
+struct Production
+{
+	char entitle;
+	char vendor;
+	int man_date;
+	int	quantity;
+	int price;
+};
+
+struct Employee
+{
+	char FIO;
+	int god_r;
+	char position;
+	char education;
+	int salary;
+};
+
+struct Student
+{
+	char FIO;
+	int group;
+	int god_r;
+	int gp_phys;
+	int gp_math;
+	int gp_ITE;
+};
+int Enuma(int strt, int fl);
+void sozd_FIO(char * name, int dlina);
+void print_Hockey(Hockey * DetroitRedWings, int i);
+void print_Tovar(Tovar * Racks, int i);
+void print_Production(Production * Magazines, int i);
+void print_Empoyee(Employee * RAMS, int i);
+void print_Student(Student * progers, int i);
